8358809413d65b0c91606e3f8d60310672823351
Zubair Ahmed <syedzubairahmed001@gmail.com>
2022-02-18 14:49:37 +0530

    removes link error

--- components/Footer/Footer.component.jsx
+++ components/Footer/Footer.component.jsx
@@ -15,7 +15,9 @@ const Footer = props => {
                 <GrPowerReset />
             </div>
             <div className={styles.appCredits}>
-                Built with <Image src="/images/heart_emoji.png" width={15} height={15} /> by <a href="https://twitter.com/zubairself" target="_blank">Zubair</a>
+                Built with 
+                <Image src="/images/heart_emoji.png" width={15} height={15} /> by 
+                <a href="https://twitter.com/zubairself" target="_blank" rel="noreferrer">Zubair</a>
             </div>
         </div>
         <div></div>
